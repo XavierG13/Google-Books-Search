5e4fc6081ed2f02a1c16ecfcc98a49bda24f200d
Xavier Guzman <guzmanxavi112@gmail.com>
2021-03-29 09:24:04 -0500

    added additional info to save to mongo db

--- models/books.js
+++ models/books.js
@@ -1,13 +1,15 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  synopsis: String,
-  date: { type: Date, default: Date.now }
+	title: { type: String, required: true },
+	author: { type: String, required: true },
+	synopsis: String,
+	date: { type: Date, default: Date.now },
+	image: String,
+	link: String,
 });
 
-const Book = mongoose.model("Book", bookSchema);
+const Book = mongoose.model('Book', bookSchema);
 
 module.exports = Book;
